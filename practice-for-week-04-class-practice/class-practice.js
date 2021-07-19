41d3718cad986154d3739736ece16c3f99afdabf
Arianna Johnson <ariannajohnson588@gmail.com>
2021-07-19 17:08:35 -0400

    working on class syntax practice

--- practice-for-week-04-class-practice/class-practice.js
+++ practice-for-week-04-class-practice/class-practice.js
@@ -1,9 +1,36 @@
 /* ============================ Phase 1 ============================ */
 // Your code here
+class Person {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+  introduce() {
+    return `Hi, I'm ${this.firstName} ${this.lastName}, and I'm ${this.age} years old.`;
+  }
+}
+
 
 /******************************************************************* */
 /* ============================ Phase 2 ============================ */
 // Your code here
+class Student extends Person {
+  constructor(firstName, lastName, age, major, GPA) {
+    super(firstName, lastName, age);
+    this.major = major;
+    this.GPA = GPA;
+  }
+
+
+}
+
+class Teacher extends Person {
+  constructor(firstName, lastName, age, subject) {
+    super(firstName, lastName, age);
+    this.subject = subject;
+  }
+}
 
 module.exports = {
   Person,
