982effb89490d7137dd2f8e10c47f65065a1204d
Arianna Johnson <ariannajohnson588@gmail.com>
2021-07-20 10:48:04 -0400

    test

--- practice-for-week-04-class-practice/class-practice.js
+++ practice-for-week-04-class-practice/class-practice.js
@@ -9,6 +9,22 @@ class Person {
   introduce() {
     return `Hi, I'm ${this.firstName} ${this.lastName}, and I'm ${this.age} years old.`;
   }
+  static introducePeople(people) {
+    if (!Array.isArray(people)) {
+      // throw new Error("introducePeople only takes an array as an argument.");
+      console.log("introducePeople only takes an array as an argument.");
+      return;
+    }
+
+    for (let person of people) {
+      if (!(person instanceof Person))
+        // throw new Error("All items in array must be Person class instances.");
+        console.log("All items in array must be Person class instances.");
+        return;
+    }
+
+    people.forEach((person) => person.introduce());
+  }
 }
 
 
