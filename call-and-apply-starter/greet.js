0129460776c56c16a96464f000f0ce4d320bb92a
Arianna Johnson <ariannajohnson588@gmail.com>
2021-07-21 12:41:43 -0400

    tic-tac-toe project

---
+++ call-and-apply-starter/greet.js
@@ -0,0 +1,10 @@
+// DO NOT MODIFY `say`
+function say(name) {
+  console.log(`${this.message} ${name}!`);
+}
+
+helloMessage = {message: 'Hello,'}
+heyThereMessage = {message: 'Hey there,'}
+
+say.call(helloMessage, "John");  // 'Hello, John'
+say.call(heyThereMessage, "Michael");   // 'Hey there, Michael'
