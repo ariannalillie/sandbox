311d85c2a75ab68b85c9f5b3656a2e3537e6728d
Arianna Johnson <ariannajohnson588@gmail.com>
2021-07-22 12:15:34 -0400

    tdd practice

--- practice-for-week-04-unit-testing-practice/class.js
+++ practice-for-week-04-unit-testing-practice/class.js
@@ -45,4 +45,6 @@ class Word {
   }
 }
 
+
+
 module.exports = { Word };
--- practice-for-week-04-unit-testing-practice/test/class-spec.js
+++ practice-for-week-04-unit-testing-practice/test/class-spec.js
@@ -6,29 +6,45 @@ const { Word } = require("../class");
 describe("Word", function () {
   describe("Word constructor function", function () {
     it('should have a "word" property', function () {
-      expect.fail("replace with your code");
+      let newWord = new Word('happy');
+
+      expect(newWord).to.have.property('word');
     });
-  
+
     it('should set the "word" property when a new word is created', function () {
-      expect.fail("replace with your code");
+      let newWord = new Word('happy');
+
+      expect(newWord.word).to.eql('happy');
     });
   });
 
   describe("removeVowels function", function () {
     it("should return a the word with all vowels removed", function () {
-      expect.fail("replace with your code");
+      let test1 = new Word('book');
+
+      const result = test1.removeVowels();
+
+      expect(result).to.eql('bk');
     });
   });
 
   describe("removeConsonants function", function () {
     it("should return the word with the consonants removed", function () {
-      expect.fail("replace with your code");
+      let test1 = new Word('book');
+
+      const result = test1.removeConsonants();
+
+      expect(result).to.eql('oo');
     });
   });
-  
+
   describe("pigLatin function", function () {
     it("should return the word converted to pig latin", function () {
-      expect.fail("replace with your code");
+      let test1 = new Word('food');
+
+      const result = test1.pigLatin();
+
+      expect(result).to.eql('oodfay');
     });
   });
 });
