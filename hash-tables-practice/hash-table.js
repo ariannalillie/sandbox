980ec3f1b61a52095cd6d958b6781a97270a2fd3
Arianna Johnson <ariannajohnson588@gmail.com>
2021-07-29 11:38:23 -0400

    hashmap solution

--- hash-tables-practice/hash-table.js
+++ hash-tables-practice/hash-table.js
@@ -21,15 +21,38 @@ class HashTable {
   }
 
   hash(key) {
-    // fill this in
+    // Given an input of any number of bits, SHA returns an output of 256 bits.
+    // Then slice is used to give us the first 8 characters of the sha256 hash.
+    const hashHexString = sha256(key).slice(0, 8);
+
+    // Convert to int
+    return parseInt(`0x${hashHexString}`);
   }
 
+  // hashMod() will be used to generate a valid integer index for the data array.
   hashMod(key) {
-    // fill this in
+    // Get index after hashing
+    return this.hash(key) % this.capacity;
   }
+
 
   insert(key, value) {
-    // fill this in
+    // Find the bucket index
+    const index = this.hashMod(key);
+    let currentPair = this.data[index];
+    let lastPair = null;
+    while (currentPair && currentPair.key !== key) {
+      lastPair = currentPair;
+      currentPair = currentPair.next;
+    }
+    if (currentPair) {
+      currentPair.value = value;
+    } else {
+      const newPair = new KeyValuePair(key, value);
+      newPair.next = this.data[index];
+      this.data[index] = newPair;
+      this.count++;
+    }
   }
 
 }
