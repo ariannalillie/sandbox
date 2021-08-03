3de4ab9ec1bb25ce36d48eea48db3d53f695ad5a
Arianna Johnson <ariannajohnson588@gmail.com>
2021-08-03 11:17:57 -0400

    long practice

--- practice-selection-sort/selection-sort.js
+++ practice-selection-sort/selection-sort.js
@@ -3,19 +3,22 @@
 function selectionSort(arr) {
 
   // Copy the original array
+  let copy = [...arr];
 
   // Create an array to store the sorted values
-
+  let store = [];
   // While the array is not empty...
-
+  while (arr.length) {
     // Do not move this console.log
     console.log(sorted.join(","));
 
     // Find the index of the minimum value in the unsorted half
-
+    
     // Save and remove the value at the min index
 
     // Add the min value to the end of the sorted array
+  }
+
 
 }
 
