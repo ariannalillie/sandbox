4fb375829d66ac016216d4a5ab611c63c7b96257
Arianna Johnson <ariannajohnson588@gmail.com>
2021-08-12 07:14:52 -0400

    dynamic array

--- practice-dynamic-array/dynamic-array.js
+++ practice-dynamic-array/dynamic-array.js
@@ -2,19 +2,14 @@ class DynamicArray {
 
   constructor(defaultSize=4) {
 
-    this.data = new Array(defaultSize);
-    this.capacity = defaultSize;
-    this.length = 0;
+    // Fill this out
 
   }
 
   read(index) {
 
-    // if the index we are looking for is >= the length then it is not in our data set
-    if (index >= this.length) return undefined;
-
-    // otherwise we want to return our array at the index we are looking for
-    return this.data[index];
+    // Fill this out
+    // You may not use any built-in JS array functions
   }
 
   unshift(val) {
