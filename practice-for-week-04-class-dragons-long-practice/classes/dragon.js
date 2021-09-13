6a0b412dd810632ce082ad34a8aeafdc0cd2721a
Arianna Johnson <ariannajohnson588@gmail.com>
2021-09-13 16:35:37 -0400

    dragon and friendly dragon

--- practice-for-week-04-class-dragons-long-practice/classes/dragon.js
+++ practice-for-week-04-class-dragons-long-practice/classes/dragon.js
@@ -1,5 +1,20 @@
 // Your code here
-
+class Dragon {
+  constructor(name, color) {
+    this.name = name;
+    this.color = color;
+  }
+  breathesFire() {
+    return `${this.name} breathes fire everywhere! BURN!!!!`
+  }
+  static getDragons(...dragons) {
+    let dragonNameArr = [];
+    dragons.forEach(dragon => {
+      dragonNameArr.push(dragon.name)
+    })
+    return dragonNameArr;
+  }
+}
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
--- practice-for-week-04-class-dragons-long-practice/classes/friendly-dragon.js
+++ practice-for-week-04-class-dragons-long-practice/classes/friendly-dragon.js
@@ -1,1 +1,19 @@
-// Your code here
+const Dragon = require('./dragon');
+
+class friendlyDragon extends Dragon {
+    constructor(name, color, friend) {
+        super(name, color);
+        this.lifeGoals = [];
+        this.friend = friend;
+    }
+    hasLifeGoals() {
+        this.lifeGoals.forEach(lifeGoal => {
+            console.log(`${this.name} likes to ${lifeGoal}`)
+        })
+    }
+    helpsPeople() {
+        return `${this.name} helps their friend ${this.friend}`
+    }
+}
+
+module.exports = friendlyDragon;
