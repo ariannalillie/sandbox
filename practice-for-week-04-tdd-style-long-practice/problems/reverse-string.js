cee7aec125c0ddddac7de25a775d2252aa747762
Arianna Johnson <ariannajohnson588@gmail.com>
2021-09-16 11:15:42 -0400

    tdd long practice

--- practice-for-week-04-tdd-style-long-practice/problems/reverse-string.js
+++ practice-for-week-04-tdd-style-long-practice/problems/reverse-string.js
@@ -1,11 +1,3 @@
-// module.exports = function reverseString(string) {
-//   let newArr = [];
-//   for (let i = string.length; i >= 0; i--) {
-//     newArr.push(string[i])
-//   }
-//   return newArr.join('');
-// };
-
 module.exports = function reverseString(string) {
-  return [...string].reverse().join("");
+  // Your code here
 };
--- practice-for-week-04-tdd-style-long-practice/test/reverse-string-spec.js
+++ practice-for-week-04-tdd-style-long-practice/test/reverse-string-spec.js
@@ -1,17 +1,1 @@
 // Your code here
-const chai = require("chai");
-const expect = chai.expect;
-const spies = require("chai-spies");
-chai.use(spies);
-
-const reverseString = require('../problems/reverse-string')
-
-describe("reverseString()", () => {
-    it("should reverse a string", () => {
-    const string1 = 'fun';
-
-    const actual1 = reverseString(string1);
-
-    expect(actual1).to.eql('nuf');
-    });
-});
