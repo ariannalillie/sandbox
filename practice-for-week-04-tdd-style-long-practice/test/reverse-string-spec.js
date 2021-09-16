5f0aaa65929b7ee3e256d8fc577f29fe11d2228f
Arianna Johnson <ariannajohnson588@gmail.com>
2021-09-16 11:24:11 -0400

    test written for reverseString

--- practice-for-week-04-tdd-style-long-practice/test/reverse-string-spec.js
+++ practice-for-week-04-tdd-style-long-practice/test/reverse-string-spec.js
@@ -1,1 +1,10 @@
-// Your code here
+const { expect } = require('chai');
+const reverseString = require('../problems/reverse-string');
+
+describe('reverseString()', function () {
+    it('Should return the reversed output of the string passed in', function () {
+        const test1 = 'fun';
+        const actual1 = reverseString(test1);
+        expect(actual1).to.eql('nuf');
+    });
+});
